65b2e0cb6c2ec9851b1c3fdd9fd11c2d395176c0
not4x217 <6069441+not4x217@users.noreply.github.com>
2025-03-18 20:45:55 +0800

    update eigensdk, refactor cli args

--- setup-operator/src/lib.rs
+++ setup-operator/src/lib.rs
@@ -1,21 +1,96 @@
+use std::{
+    fs::File,
+    path::Path,
+    str::FromStr,
+};
+
+use anyhow::Result;
 use clap::Parser;
+
+use alloy_primitives::Address;
+use alloy_signer_local::PrivateKeySigner;
+
+use eigen_crypto_bls::BlsKeyPair;
+
 #[derive(Parser, Clone, Debug)]
 pub struct Options {
+    #[clap(long, env = "EL_DEPLOYMENT_FILE_PATH")]
+    pub el_deployment_file_path: String,
+
+    #[clap(long, env = "AVS_DEPLOYMENT_FILE_PATH")]
+    pub avs_deployment_file_path: String,
+
     #[clap(long, env = "OPERATOR_PRIVATE_KEY")]
     pub operator_private_key: String,
 
     #[clap(long, env = "OPERATOR_BLS_KEY")]
     pub operator_bls_key: String,
 
-    #[clap(long, env = "HTTP_ENDPOINT")]
-    pub http_endpoint: String,
+    #[clap(long, env = "OPERATOR_SOCKET")]
+    pub operator_socket: String,
 
-    #[clap(long, env = "CHAIN_WRITER_READER_ADDRESSES")]
-    pub chain_writer_reader_addresses: String,
+    #[clap(long, env = "EL_NODE_URL")]
+    pub el_node_url: String,
+}
 
-    #[clap(long, env = "AVS_REGISTRY_WRITER_ADDRESSES")]
-    pub avs_registry_writer_addresses: String,
+impl Options {
+    pub fn el_deployment(&self) -> Result<EigenlayerDeployment> {
+        let deployment_file = File::open(Path::new(&self.el_deployment_file_path))?;
+        let deployment: EigenlayerDeployment = serde_json::from_reader(deployment_file)?;
+        Ok(deployment)
+    }
 
-    #[clap(long, env = "SOCKET")]
-    pub socket: String,
+    pub fn avs_deployment(&self) -> Result<AvsDeployment> {
+        let deployment_file = File::open(Path::new(&self.avs_deployment_file_path))?;
+        let deployment: AvsDeployment = serde_json::from_reader(deployment_file)?;
+        Ok(deployment)
+    }
+
+    pub fn signer(&self) -> Result<PrivateKeySigner> {
+        let signer = PrivateKeySigner::from_str(&self.operator_private_key)?;
+        Ok(signer)
+    }
+
+    pub fn bls_key_pair(&self) -> Result<BlsKeyPair> {
+        let key_pair = BlsKeyPair::new(self.operator_bls_key.to_string())?;
+        Ok(key_pair)
+    }
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct EigenlayerDeployment {
+    pub allocation_manager: Address,
+    pub avs_directory: Address,
+    pub delegation_manager: Address,
+    pub eigen_pod_beacon: Address,
+    pub eigen_pod_manager: Address,
+    pub pauser_registry: Address,
+    pub permission_controller: Address,
+    pub proxy_admin: Address,
+    pub rewards_coordinator: Address,
+    pub strategy_beacon: Address,
+    pub strategy_factory: Address,
+    pub strategy_manager: Address,
+}
+
+#[derive(serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AvsDeployment {
+    pub L1_sender: Address,
+    pub L2_coprocessor: Address,
+    pub L2_coprocessor_caller: Address,
+    pub bls_apk_registry: Address,
+    pub coprocessor: Address,
+    pub coprocessor_service_manager: Address,
+    pub coprocessor_to_L2: Address,
+    pub index_registry: Address,
+    pub operator_state_retriever: Address,
+    pub pauser_registry: Address,
+    pub proxy_admin: Address,
+    pub registry_coordinator: Address,
+    pub socket_registry: Address,
+    pub stake_registry: Address,
+    pub strategy: Address,
+    pub strategy_token: Address
 }
--- setup-operator/src/main.rs
+++ setup-operator/src/main.rs
@@ -9,20 +9,59 @@ use eigen_client_elcontracts::{
 use eigen_crypto_bls::BlsKeyPair;
 use eigen_logging::get_test_logger;
 use eigen_testing_utils::transaction::wait_transaction;
-use eigen_utils::erc20::ERC20;
-use eigen_utils::get_signer;
-use eigen_utils::strategymanager::StrategyManager;
+//use eigen_utils::erc20::ERC20;
+use eigen_common::get_signer;
+use eigen_utils::slashing::{
+    core::strategymanager::StrategyManager,
+    middleware::ierc20::IERC20::{self, IERC20Instance},
+};
 use setup_operator::Options;
 use std::{
     fs::File,
     path::Path,
+    process::exit,
     str::FromStr,
     time::{SystemTime, UNIX_EPOCH},
 };
 
 #[tokio::main]
 async fn main() {
-    let opt = Options::parse();
+    let opts = Options::parse();
+    
+    let el_deployment = match opts.el_deployment() {
+        Ok(deployment) => deployment,
+        Err(err) => {
+            eprintln!("invalid eigenlayer deployment: {}", err);
+            exit(1)
+        }
+    };
+
+    let avs_deployment = match opts.avs_deployment() {
+        Ok(deployment) => deployment,
+        Err(err) => {
+            eprintln!("invalid avs deployment: {}", err);
+            exit(1)
+        }
+    };
+
+    let signer = match opts.signer() {
+        Ok(signer) => signer,
+        Err(err) => {
+            eprintln!("invalid operator private key: {}", err);
+            exit(1)
+        }
+    };
+
+    let bls_key_pair = match opts.bls_key_pair() {
+        Ok(key_pair) => key_pair,
+        Err(err) => {
+            eprintln!("invalid operator bls key: {}", err);
+            exit(1)
+        }
+    };
+
+    // !!!
+    /*
     let signer = PrivateKeySigner::from_str(&opt.operator_private_key).unwrap();
 
     let deployment_parameters_devnet =
@@ -30,58 +69,73 @@ async fn main() {
 
     let json: serde_json::Value = serde_json::from_reader(deployment_parameters_devnet).unwrap();
 
-    let delegation_manager_address = Address::parse_checksummed(
-        json.get("delegationManager").unwrap().as_str().unwrap(),
-        None,
-    )
-    .unwrap();
+    let delegation_manager_address = 
+        Address::parse_checksummed(json.get("delegationManager").unwrap().as_str().unwrap(), None)
+            .unwrap();
 
     let avs_directory_address =
         Address::parse_checksummed(json.get("avsDirectory").unwrap().as_str().unwrap(), None)
             .unwrap();
 
+    let allocation_manager_address =
+        Address::parse_checksummed(json.get("allocationManager").unwrap().as_str().unwrap(), None)
+            .unwrap();
+    
     let strategy_manager_address =
         Address::parse_checksummed(json.get("strategyManager").unwrap().as_str().unwrap(), None)
             .unwrap();
 
     let rewards_coordinator_address =
-        Address::parse_checksummed(json.get("avsDirectory").unwrap().as_str().unwrap(), None)
+        Address::parse_checksummed(json.get("rewardsCoordinator").unwrap().as_str().unwrap(), None)
             .unwrap();
 
-    let el_chain_reader = ELChainReader::new(
+    let permission_controller_address =
+        Address::parse_checksummed(json.get("permissionController").unwrap().as_str().unwrap(), None)
+            .unwrap();
+    */
+
+    let el_reader = ELChainReader::new(
         get_test_logger(),
-        Address::ZERO,
-        delegation_manager_address,
-        avs_directory_address,
-        opt.http_endpoint.to_owned(),
+        Some(el_deployment.allocation_manager),
+        el_deployment.delegation_manager,
+        el_deployment.rewards_coordinator,
+        el_deployment.avs_directory,
+        Some(el_deployment.permission_controller),
+        opts.el_node_url.to_owned(),
     );
 
-    let el_chain_writer = ELChainWriter::new(
-        delegation_manager_address,
-        strategy_manager_address,
-        rewards_coordinator_address,
-        el_chain_reader.clone(),
-        opt.http_endpoint.to_string(),
-        opt.operator_private_key.to_string(),
+    let el_writer = ELChainWriter::new(
+        el_deployment.strategy_manager,
+        el_deployment.rewards_coordinator,
+        Some(el_deployment.permission_controller),
+        Some(el_deployment.allocation_manager),
+        avs_deployment.registry_coordinator,
+        el_reader.clone(),
+        opts.el_node_url.to_string(),
+        opts.operator_private_key.to_string(),
     );
 
     let operator_details = Operator {
         address: signer.address(),
-        earnings_receiver_address: signer.address(),
         delegation_approver_address: signer.address(),
-        staker_opt_out_window_blocks: 3,
-        metadata_url: Some("eigensdk-rs".to_string()),
+        metadata_url: "eigensdk-rs".to_string(),
+        allocation_delay: None,
+        _deprecated_earnings_receiver_address: Some(signer.address()),
+        staker_opt_out_window_blocks: Some(3),
     };
 
+    // !!!
     /* let _ = el_chain_writer
         .register_as_operator(operator_details)
         .await
         .unwrap();
     */
 
+    // !!!
+    /* 
     let provider = get_signer(
         &opt.operator_private_key.to_string(),
-        &opt.http_endpoint.to_string(),
+        &opt.el_node_url.to_string(),
     );
 
     let coprocessor_deployment_output_devnet =
@@ -111,33 +165,38 @@ async fn main() {
         None,
     )
     .unwrap();
-    let avs_registry_writer = AvsRegistryChainWriter::build_avs_registry_chain_writer(
-        get_test_logger(),
-        opt.http_endpoint.to_string(),
-        opt.operator_private_key.to_string(),
-        registry_coordinator,
-        operator_state_retriever,
-    )
-    .await
-    .unwrap();
+    */
 
+    let avs_writer = AvsRegistryChainWriter::build_avs_registry_chain_writer(
+        get_test_logger(),
+        opts.el_node_url.to_string(),
+        opts.operator_private_key,
+        avs_deployment.registry_coordinator,
+        avs_deployment.operator_state_retriever,
+    ).await.unwrap();
+
+    // !!!
+    /*
     let bls_key_pair = BlsKeyPair::new(opt.operator_bls_key.to_string()).unwrap();
     let salt: FixedBytes<32> = FixedBytes::from([0x03; 32]);
     let now = SystemTime::now();
     let seconds_since_epoch = now.duration_since(UNIX_EPOCH).unwrap().as_secs();
     let expiry = U256::from(seconds_since_epoch) + U256::from(10000);
     let quorum_numbers = Bytes::from_str("0x00").unwrap();
-
-    let tx_hash = avs_registry_writer
+    
+    let tx_hash = avs_writer
         .register_operator_in_quorum_with_avs_registry_coordinator(
             bls_key_pair,
             salt,
             expiry,
             quorum_numbers,
-            opt.socket,
+            opts.operator_socket,
         )
         .await
         .unwrap();
-    wait_transaction(&opt.http_endpoint, tx_hash).await.unwrap();
+    wait_transaction(&opts.el_node_url, tx_hash).await.unwrap();
     println!("setup_operator finished");
+    */
 }
+
+
